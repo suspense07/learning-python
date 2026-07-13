b1e8310aae231b0a3e177b6b61d147f3ad9336b0
suspense07 <2150098542@qq.com>
2026-07-13 21:11:04 +0800

    feat：记账小程序加文件存储

--- 03_expense_tracker/expense_traker.py
+++ 03_expense_tracker/expense_traker.py
@@ -2,16 +2,36 @@
 记账小程序：
 1.创建记账列表
 2.添加记账本功能（1.添加消费，2.查看所有消费，3.按类别统计，4.退出）
+3.添加文件存储功能(1.读取文件，2.写入文件)
 '''
 
 import ast
+import os
+import json
 
 
-book = []
 rough_book = {}
+
+# 读取文件
+if os.path.exists('tally_book.json'):
+    with open('tally_book.json', 'r', encoding='utf-8') as f:
+        book = json.load(f)
+    for i in book:
+        consume_category = i[0]
+        money = i[1]
+        if consume_category not in rough_book.keys():
+            rough_book[consume_category] = money
+        else:
+            rough_book[consume_category] += money
+else:
+    book = []
+
+
+
 
 while True:
     user_command = (input('\n====== 记账本 ======\n1.添加消费\n2.查看所有消费\n3.按类别统计\n4.退出\n请输入对应数字：'))
+
     if user_command == '1':
         consume_category = input('请输入消费类别：')
         money = ast.literal_eval(input('请输入金额：'))
@@ -20,6 +40,10 @@ while True:
             rough_book[consume_category] = money
         else:
             rough_book[consume_category] += money
+        # 写入文件
+        with open('tally_book.json', 'w', encoding='utf-8') as f:
+            json.dump(book, f, ensure_ascii=False, indent=2)
+
     elif user_command == '2':
         print('=== 详细消费 ===')
         for i,cost_tup in enumerate(book):
